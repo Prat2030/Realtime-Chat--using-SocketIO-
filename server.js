de3bbd747ca96a06b3c03ad2adac3a3089793a42
Prat2030 <pratyushjindal2003@gmail.com>
2023-04-12 16:57:49 +0530

    add functions to room and chat join

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,17 +10,31 @@ const io = socketio(server);
 
 // Set Static folder
 app.use(express.static(path.join(__dirname, 'public')));
+const botName = 'ChatBot';
 const PORT = process.env.PORT || 4000;
 
 // Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
 
-    socket.emit('message', 'Welcome to RealChat!');
+    // Join room
+    socket.on('joinRoom', ({ username, room }) => {
+      // Welcome current user
+      socket.emit('message', formatMessage(botName,'Welcome to RealChat!'));
 
-    // socket.emit -> to the current single client ; io.emit -> to all the clients ; socket.broadcast.emit -> to all the clients except the current one
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+      // socket.emit -> to the current single client ; io.emit -> to all the clients ; socket.broadcast.emit -> to all the clients except the current one
+      // Broadcast when a user connects
+      socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER',msg));
+    });
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+      io.emit('message', formatMessage(botName,'A user has left the chat'));
+  });
 });
 
 
